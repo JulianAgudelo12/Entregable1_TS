72e4c4ef6d276790881cfbdca0e3828dcb9074d8
valchys <vcorrales1@eafit.edu.co>
2025-03-22 19:02:01 -0500

    fix: modify User table for admin verification, User model getter and setters

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,23 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    /**
+     * USER ATTRIBUTES
+     * $this->attributes['id'] - int - contains the user primary key (id)
+     * $this->attributes['name'] - string - contains the user name
+     * $this->attributes['email'] - string - contains the user email
+     * $this->attributes['email_verified_at'] - timestamp - contains the user email verification date
+     * $this->attributes['password'] - string - contains the user password
+     * $this->attributes['remember_token'] - string - contains the user authentication token
+     * $this->attributes['created_at'] - timestamp - contains the user creation date
+     * $this->attributes['updated_at'] - timestamp - contains the user update date
+     * $this->attributes['is_admin'] - boolean - represents the user role as admin (true) or client (false).
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     protected $hidden = [
@@ -28,5 +41,50 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getId(): string
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function getIsadmin(): bool
+    {
+        return $this->attributes['is_admin'];
+    }
+
+    public function setIsadmin(bool $isAdmin): void
+    {
+        $this->attributes['is_admin'] = $isAdmin;
     }
 }
