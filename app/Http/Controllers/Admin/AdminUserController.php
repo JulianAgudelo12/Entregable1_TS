f6ed761b6b5884bdf9e4a51a9435c19c7b2fc80d
valchys <vcorrales1@eafit.edu.co>
2025-03-24 05:05:32 -0500

    feat: add Edit and Delete User funcionalities for admins

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -6,6 +6,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Utilities\UserHelper;
+use App\Utilities\UserValidator;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class AdminUserController extends Controller
@@ -20,5 +24,34 @@ class AdminUserController extends Controller
         $viewData['totalUsers'] = User::all()->count();
 
         return view('admin.user.index')->with('viewData', $viewData);
+    }
+
+    public function destroy(string $id): RedirectResponse
+    {
+        User::destroy($id);
+
+        return redirect()->route('admin.user.index')->with('success', 'User deleted successfully!');
+    }
+    
+    public function edit(string $id): View
+    {
+        $user = User::findOrFail($id);
+
+        $viewData = [];
+        $viewData['title'] = 'Edit User';
+        $viewData['user'] = $user;
+
+        return view('admin.user.edit')->with('viewData', $viewData);
+    }
+
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        UserValidator::validate($request->all());
+
+        $user = User::findOrFail($id);
+        UserHelper::fillUserData($user, $request);
+        $user->save();
+
+        return redirect()->route('admin.user.index')->with('success', 'User Updated successfully!');
     }
 }
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,8 +3,16 @@
 @section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
 @section('content')
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+    @foreach ($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+    </ul>
+</div>
+@endif
     <h3>Users ({{ $viewData["totalUsers"] }})</h3>
-    
     <table class="table">
         <thead>
             <tr>
@@ -12,6 +20,9 @@
                 <th>Name</th>
                 <th>Email</th>
                 <th>Created At</th>
+                <th>Updated At</th>
+                <th>Role</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -21,6 +32,16 @@
                     <td>{{ $user->getName() }}</td>
                     <td>{{ $user->getEmail() }}</td>
                     <td>{{ $user->getCreatedAt() }}</td>
+                    <td>{{ $user->getUpdatedAt() }}</td>
+                    <td>{{ $user->getIsAdmin() ? 'Admin' : 'Client' }}</td>
+                    <td>
+                        <a href="{{ route('admin.user.edit', ['id'=> $user->getId()]) }}" class="btn btn-warning btn-sm">Edit</a>
+                        <form action="{{ route('admin.user.destroy', ['id' => $user->getId()]) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this User?')">Delete</button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
