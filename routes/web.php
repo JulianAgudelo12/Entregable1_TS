6e15cf7738cbb27bd997e43fcda4b01995c7d808
valchys <vcorrales1@eafit.edu.co>
2025-03-23 19:02:15 -0500

    fix: define paths as variables

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+$HomeControllerPath ='App\Http\Controllers\HomeController';
+
 Auth::routes();
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
+Route::get('/', "$HomeControllerPath@index")->name('home.index');
+
 Route::middleware('admin')->group(function () {
-    Route::get('/admin', 'App\Http\Controllers\Admin\AdminDashboardController@index')->name('admin.dashboard.index');
-    Route::get('/admin/users', 'App\Http\Controllers\Admin\AdminUserController@index')->name('admin.users.index');
+    $AdminDashboardControllerPath ='App\Http\Controllers\Admin\AdminDashboardController';
+    $AdminUserCOntrollerPath = 'App\Http\Controllers\Admin\AdminUserController';
+
+    Route::get('/admin', "$AdminDashboardControllerPath@index")->name('admin.dashboard.index');
+    Route::get('/admin/users', "$AdminUserCOntrollerPath@index")->name('admin.users.index');
 });
