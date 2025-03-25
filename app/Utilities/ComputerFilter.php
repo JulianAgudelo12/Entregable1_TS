6db6f2369dc8cd9586e952bf6e08e702e6eba8f2
valchys <vcorrales1@eafit.edu.co>
2025-03-25 01:22:13 -0500

    feat: special functionalities, general fixes

---
+++ app/Utilities/ComputerFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Utilities;
+
+use App\Models\Computer;
+use Illuminate\Http\Request;
+
+class ComputerFilter
+{
+    public static function apply(Request $request)
+    {
+        $query = Computer::query();
+
+        if ($request->filled('name')) {
+            $query->where('name', 'like', '%'.$request->name.'%');
+        }
+
+        if ($request->filled('brand')) {
+            $query->where('brand', 'like', '%'.$request->brand.'%');
+        }
+
+        if ($request->filled('min_price')) {
+            $query->where('price', '>=', $request->min_price);
+        }
+
+        if ($request->filled('max_price')) {
+            $query->where('price', '<=', $request->max_price);
+        }
+
+        return $query;
+    }
+}
